9f32fe48eb0f3a4c605d51da37614855b8bdde87
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-06 00:07:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -409,7 +409,7 @@ class Test {
   }
 
   _teardown (fn, opts) {
-    if (this.isDone) throw new Error('Can\'t add teardown after end')
+    if (this.isEnded || this.isDone) throw new Error('Can\'t add teardown after end')
     this._teardowns.push([(opts && opts.order) || 0, fn])
   }
 
