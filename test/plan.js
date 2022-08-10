f423bb52c2e0fc7018d0572a4bc4427cd76075cd
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-10 18:33:57 -0300

    using original output as expected

--- test/plan.js
+++ test/plan.js
@@ -8,16 +8,27 @@ test(async function (t) {
     },
     `
     TAP version 13
-    not ok 1 - plan takes a positive whole number only
-    ---
-    operator: plan
-    at:
-    line: 4
-    column: 9
-    stack: |
-    ...
-    not ok 1 - plan must be positive
+
+    # plan must be positive
+        not ok 1 - plan takes a positive whole number only
+          ---
+          operator: plan
+          at: 
+            line: 4
+            column: 9
+            file: /[eval]
+          stack: |
+            [eval]:4:9
+            processTicksAndRejections (node:internal/process/task_queues:96:5)
+          ...
+    not ok 1 - plan must be positive # time = 4.043233ms
+
     1..1
+    # tests = 0/1 pass
+    # asserts = 0/1 pass
+    # time = 7.180862ms
+
+    # not ok
     `
   )
 })
@@ -35,25 +46,41 @@ test(async function (t) {
     },
     `
     TAP version 13
-    not ok 1 - plan takes a positive whole number only
-    ---
-    operator: plan
-    at:
-    line: 4
-    column: 11
-    stack: |
-    ...
-    not ok 1 - plan must be integer
-    not ok 1 - plan takes a positive whole number only
-    ---
-    operator: plan
-    at:
-    line: 8
-    column: 11
-    stack: |
-    ...
-    not ok 2 - plan must be positive
+
+    # plan must be integer
+        not ok 1 - plan takes a positive whole number only
+          ---
+          operator: plan
+          at: 
+            line: 4
+            column: 11
+            file: /[eval]
+          stack: |
+            [eval]:4:11
+            processTicksAndRejections (node:internal/process/task_queues:96:5)
+          ...
+    not ok 1 - plan must be integer # time = 4.137088ms
+
+    # plan must be positive
+        not ok 1 - plan takes a positive whole number only
+          ---
+          operator: plan
+          at: 
+            line: 8
+            column: 11
+            file: /[eval]
+          stack: |
+            [eval]:8:11
+            processTicksAndRejections (node:internal/process/task_queues:96:5)
+          ...
+    not ok 2 - plan must be positive # time = 0.359364ms
+
     1..2
+    # tests = 0/2 pass
+    # asserts = 0/2 pass
+    # time = 8.126944ms
+
+    # not ok
     `
   )
 })
