b6a531aba372b8d22ccc85d83a02d43bf1066f11
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-10 18:19:17 -0300

    simpler comparison using tester/spawner

--- test/plan.js
+++ test/plan.js
@@ -1,70 +1,59 @@
 const test = require('../')
-const { spawner, tester, standardizeTap } = require('./helpers')
+const { spawner, tester } = require('./helpers')
 
 test(async function (t) {
-  const std = await spawner(function () {
-    test('plan must be integer', function (t) {
-      t.plan('x')
-    })
-
-    test('plan must be positive', function (t) {
+  await tester(t, 'plan must be positive',
+    function (t) {
       t.plan(-1)
-    })
-  })
-
-  t.is(std.stderr, '')
-  t.is(std.stdout, standardizeTap(`
-  TAP version 13
+    },
+    `
+    TAP version 13
     not ok 1 - plan takes a positive whole number only
-      ---
-      operator: plan
-      at:
-        line: 4
-        column: 9
-        file: /[eval]
-      stack: |
-        [eval]:4:9
-      ...
-  not ok 1 - plan must be integer
-      not ok 1 - plan takes a positive whole number only
-        ---
-        operator: plan
-        at:
-          line: 8
-          column: 9
-          file: /[eval]
-        stack: |
-          [eval]:8:9
-          processTicksAndRejections (node:internal/process/task_queues:96:5)
-        ...
-  not ok 2 - plan must be positive
-  1..2
-  `))
-
-  t.pass()
+    ---
+    operator: plan
+    at:
+    line: 4
+    column: 9
+    stack: |
+    ...
+    not ok 1 - plan must be positive
+    1..1
+    `
+  )
 })
 
 test(async function (t) {
-  const std = await tester('plan must be positive', function (t) {
-    t.plan(-1)
-  })
+  await spawner(t,
+    function () {
+      test('plan must be integer', function (t) {
+        t.plan('x')
+      })
 
-  t.is(std.stderr, '')
-  t.is(std.stdout, standardizeTap(`
-  TAP version 13
+      test('plan must be positive', function (t) {
+        t.plan(-1)
+      })
+    },
+    `
+    TAP version 13
     not ok 1 - plan takes a positive whole number only
-      ---
-      operator: plan
-      at:
-        line: 4
-        column: 7
-        file: /[eval]
-      stack: |
-        [eval]:4:7
-      ...
-  not ok 1 - plan must be positive
-  1..1
-  `))
-
-  t.pass()
+    ---
+    operator: plan
+    at:
+    line: 4
+    column: 11
+    stack: |
+    ...
+    not ok 1 - plan must be integer
+    not ok 1 - plan takes a positive whole number only
+    ---
+    operator: plan
+    at:
+    line: 8
+    column: 11
+    stack: |
+    ...
+    not ok 2 - plan must be positive
+    1..2
+    `
+  )
 })
