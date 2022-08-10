f933827795a0a46490cff4916a90a7c27bc647e1
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-10 15:09:03 -0300

    simpler spawner

---
+++ test/plan.js
@@ -0,0 +1,14 @@
+const test = require('../')
+const { spawner } = require('./helper.js')
+
+test(async function (t) {
+  const std = await spawner(() => {
+    test('plan must be positive', function (t) {
+      t.plan(-1)
+    })
+  })
+
+  t.ok(std.stdout.indexOf('not ok 1 - plan takes a positive whole number only') > -1)
+  t.is(std.stderr, undefined)
+  t.pass()
+})
