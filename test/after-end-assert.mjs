61522f45a645daf1f4277d7b4b583f94c2b3532e
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-11 14:41:06 -0300

    Update after-end-assert.mjs

--- test/after-end-assert.mjs
+++ test/after-end-assert.mjs
@@ -11,18 +11,18 @@ await tester('assert after end',
 
   # assert after end
       ok 1 - passed
-  ok 1 - assert after end # time = 0.762984ms
       ok 2 - passed
-  not ok 2 - assertion after end (in assert after end)
+      not ok 3 - assertion after end
+  not ok 1 - assert after end # time = 0.717958ms
 
-  1..2
-  # tests = 1/2 pass
-  # asserts = 2/2 pass
-  # time = 3.777711ms
+  1..1
+  # tests = 0/1 pass
+  # asserts = 2/3 pass
+  # time = 4.005746ms
 
   # not ok
   `,
-  { exitCode: 0 }
+  { exitCode: 1 }
 )
 
 await spawner(
@@ -34,17 +34,17 @@ await spawner(
   },
   `
   # assert after end
-  ok 1 - passed
-  ok 1 - assert after end # time = 0.757493ms
+      ok 1 - passed
+  ok 1 - assert after end # time = 0.629102ms
       ok 2 - passed
   not ok 2 - assertion after end (in assert after end)
 
   1..2
   # tests = 1/2 pass
   # asserts = 2/2 pass
-  # time = 3.322585ms
+  # time = 3.555015ms
 
   # not ok
   `,
-  { exitCode: 0 }
+  { exitCode: 1 }
 )
